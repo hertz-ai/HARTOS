db0b988133af99532d47758d5866ca7ca3cc3f04
Garv Soni <garvsoni@Garvs-MacBook-Air.local>
2024-04-23 15:47:15 +0530

    req.txt and start.sh change

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
 #!bin/bash
-pip install --upgrade pip
-docker build -t langchain_gpt:v1 .
-docker kill langchain
-docker rm langchain
-docker run -dp 5055:5000 --network host --name langchain langchain_gpt:v1
+# pip install --upgrade pip
+# docker build -t langchain_gpt:v1 .
+# docker kill langchain
+# docker rm langchain
+# docker run -dp 5055:5000 --network host --name langchain langchain_gpt:v1
 
 
-# sudo docker build -t langchain_gpt:v1 .
-# sudo docker kill langchain
-# sudo docker rm langchain
-# sudo docker run -dp 5000:5000 --network host --name langchain langchain_gpt:v1
+sudo pip install --upgrade pip
+sudo docker build -t langchain_gpt1:v1 .
+sudo docker kill langchain
+sudo docker rm langchain
+sudo docker run -dp 5000:5000 --network host --name langchain langchain_gpt1:v1
